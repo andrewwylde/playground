3c85f99a7c0bdb26719c68dcf06fe66f802be315
Andrew Ellis <astoellis@gmail.com>
2017-01-17 10:22:58 -0500

    generate dynamic nav list

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import { log } from './lib';
-console.log( log );
+import pages from './pages';
 
-document.addEventListener( 'DOMContentLoaded', () => {
+window.addEventListener( 'load', () => {
   const hack = document.createElement('h1');
-  hack.textContent = 'FUCK YOUUUUUUUUUUUUU H1111111';
+  hack.textContent = 'Andrew\'s Playground';
+
   document.body.appendChild( hack );
-})
+  const navList = document.createElement('ul');
+  navList.classList.add('nav');
+
+
+  for ( let { name } of pages ) {
+    const navEl = document.createElement('li');
+    const navLink = document.createElement('a');
+    navLink.textContent = name;
+    navLink.href = `/dist/${name}.html`;
+
+    navEl.append( navLink );
+    navList.append( navEl );
+
+
+  }
+  document.body.appendChild( navList );
+});
